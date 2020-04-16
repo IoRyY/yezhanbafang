1ec1e0386d909b5fce52780a21b1be80f7ce56f8
IoRyY <253625488@qq.com>
2020-04-17 07:53:55 +0800

    添加项目文件。

---
+++ standard/yezhanbafang.Core/yezhanbafangCore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace yezhanbafang.Core
+{
+    public class yezhanbafangCore
+    {
+    }
+
+    #region 枚举
+    /// <summary>
+    /// 链接类型
+    /// </summary>
+    public enum ConType
+    {
+        /// <summary>
+        /// 空
+        /// </summary>
+        Null,
+        /// <summary>
+        /// sql
+        /// </summary>
+        Sql,
+        /// <summary>
+        /// access
+        /// </summary>
+        Access,
+        /// <summary>
+        /// Oracle
+        /// </summary>
+        Oracle,
+        /// <summary>
+        /// MySQL
+        /// </summary>
+        MySQL,
+        /// <summary>
+        /// Excel
+        /// </summary>
+        Excel
+    }
+
+    #endregion
+}
