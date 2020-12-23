6ccda9905bbbfa711eccb07d5d4a61022b86eae9
东辉 袁 <windy_23762872@126.com>
2020-12-23 17:09:08 +0800

    增加dataTable转xml

--- framework/yezhanbafang.fw.Core/IoRyClass.cs
+++ framework/yezhanbafang.fw.Core/IoRyClass.cs
@@ -1149,6 +1149,37 @@ namespace yezhanbafang.fw.Core
 
         #region 字节转化，压缩
 
+
+        /// <summary>
+        /// datatable序列化string
+        /// </summary>
+        /// <param name="pDt"></param>
+        /// <returns></returns>
+        public static string SerializeDataTableXml(DataTable pDt)
+        {
+            //  序列化DataTable
+            pDt.TableName = pDt.TableName;
+            StringBuilder sb = new StringBuilder();
+            XmlWriter writer = XmlWriter.Create(sb);
+            XmlSerializer serializer = new XmlSerializer(typeof(DataTable));
+            serializer.Serialize(writer, pDt);
+            writer.Close();
+            return sb.ToString();
+        }
+        ///   <summary>
+        ///  反序列化DataTable string
+        ///   </summary>
+        ///   <param name="pXml"> 序列化的DataTable </param>
+        ///   <returns> DataTable </returns>
+        public static DataTable DeserializeDataTable(string pXml)
+        {
+            StringReader strReader = new StringReader(pXml);
+            XmlReader xmlReader = XmlReader.Create(strReader);
+            XmlSerializer serializer = new XmlSerializer(typeof(DataTable));
+            DataTable dt = serializer.Deserialize(xmlReader) as DataTable;
+            return dt;
+        }
+
         /// <summary>
         /// 本地文件转化成Base64
         /// </summary>
